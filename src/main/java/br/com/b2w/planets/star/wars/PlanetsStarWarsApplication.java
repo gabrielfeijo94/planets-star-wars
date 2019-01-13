9845313d4b292712936d0864ca0e76ae401d005d
Gabriel Feijó <gabrielfeijo94@gmail.com>
2019-01-13 00:47:36 -0200

    Primeiro commit - projeto criado

---
+++ src/main/java/br/com/b2w/planets/star/wars/PlanetsStarWarsApplication.java
@@ -0,0 +1,14 @@
+package br.com.b2w.planets.star.wars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetsStarWarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetsStarWarsApplication.class, args);
+	}
+
+}
+
